6f43930bed95d04fad4ce0186b44cdee0c34e2ea
Nikkita Bersan <nbersan@crane-cam.epfl.pix4d>
2020-01-24 07:08:19 +0000

    Project

--- erpnext/projects/doctype/project/project_list.js
+++ erpnext/projects/doctype/project/project_list.js
@@ -1,11 +1,19 @@
 frappe.listview_settings['Project'] = {
 	add_fields: ["status", "priority", "is_active", "percent_complete", "expected_end_date", "project_name"],
-	filters:[["status","=", "Open"]],
+	//filters:[["status","=", "Open"]],
 	get_indicator: function(doc) {
-		if(doc.status=="Open" && doc.percent_complete) {
+		return [__(doc.status), {
+			"Open": "red",
+			"Cameras Assigned": "orange",
+			"In Progress": "yellow",
+			"Cameras Return": "blue",
+			"Completed": "green",
+			"Cancelled": "red"
+		}
+/*		if(doc.status=="Open" && doc.percent_complete) {
 			return [__("{0}%", [cint(doc.percent_complete)]), "orange", "percent_complete,>,0|status,=,Open"];
 		} else {
 			return [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];
-		}
+		}*/
 	}
 };
