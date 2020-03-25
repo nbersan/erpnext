ddde2516e203bb5046d8fd377c6214e43c1bea96
Nikkita Bersan <nbersan@crane-cam.epfl.pix4d>
2020-03-25 08:15:22 +0000

    stock_dashboard

--- erpnext/stock/page/cc_production/cc_production.py
+++ erpnext/stock/page/cc_production/cc_production.py
@@ -1,16 +1,14 @@
 import jwt
-import time
 
 METABASE_SITE_URL = "http://crane-cam:3000"
 METABASE_SECRET_KEY = "2395ab3fe3f8c0720057ff8327dbfbddbc7eae49bfb6d3b1bd1e625e65381f86"
 
 payload = {
-  "resource": {"dashboard": 1},
+  "resource": {"dashboard": 2},
   "params": {
     
-  },
-  "exp": round(time.time()) + (60 * 10) # 10 minute expiration
+  }
 }
 token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
 
-iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token.decode("utf8") + "#bordered=true&titled=false"
+iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token.decode("utf8") + "#bordered=false&titled=false"
