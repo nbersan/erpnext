b7e8d40999e24e53e597dcfb173e973865ef0195
Nikkita Bersan <nbersan@crane-cam.epfl.pix4d>
2020-03-31 14:14:09 +0200

    test

--- erpnext/quality_management/doctype/testbench_results/testbench_results.js
+++ erpnext/quality_management/doctype/testbench_results/testbench_results.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2020, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('TestBench Results', {
-	// refresh: function(frm) {
+let it_code;
+let version;
+cur_frm.add_fetch("camera_serial", "cam_port", "camera_port")
 
-	// }
+frappe.ui.form.on('TestBench Results', {
+//	refresh: function(frm) {
+//	}
 });
