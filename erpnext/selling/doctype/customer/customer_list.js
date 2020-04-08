aee328b8e33260ba96643c7bb4359fb8fbe3d045
Nikkita Bersan <nbersan@crane-cam.epfl.pix4d>
2020-04-08 13:13:45 +0200

    customer

--- erpnext/selling/doctype/customer/customer_list.js
+++ erpnext/selling/doctype/customer/customer_list.js
@@ -1,4 +1,4 @@
 frappe.listview_settings['Customer'] = {
-	add_fields: ["customer_name", "territory", "customer_group", "customer_type", "image"],
+	add_fields: ["customer_name", "territory", "customer_group", "customer_type", "image", "disabled"],
 	filters: [["disabled", "=", "0"]],
 };
